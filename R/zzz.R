c14d5c76b673cc2bf6bbcb3ce02ea0ee240910e0
Stéphane Laurent <laurent_step@outlook.fr>
2023-08-17 06:52:39 +0000

    version 1.0.0

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+#' @importFrom pingr is_online
+#' @importFrom pingr is_up
+NULL
+
+.onLoad <- function(libname, pkgname) {
+
+  fourier_is_up <- pingr::is_up("www-fourier.univ-grenoble-alpes.fr")
+  no_giac       <- !.giac_is_installed()
+
+  if(fourier_is_up && no_giac) {
+    .giac_download()
+  }
+
+}
